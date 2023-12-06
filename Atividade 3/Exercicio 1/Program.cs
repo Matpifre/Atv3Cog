2c34569cf12c214dafcaf45e80f9b389f67743a6
Mateus Pinho <86007159+Matpifre@users.noreply.github.com>
2023-12-05 23:43:24 -0300

    first commit

---
+++ Atividade 3/Exercicio 1/Program.cs
@@ -0,0 +1,50 @@
+﻿using SkiaSharp;
+
+namespace Projeto {
+	class Program {
+		static void Main(string[] args) {
+
+			int imagens;
+
+			Console.WriteLine("Quantas imagens você deseja analisar?");
+            imagens = Convert.ToInt32(Console.ReadLine());
+			Dictionary<string, int> objetos = new Dictionary<string, int>(imagens);
+			for (int i = 0 ; i < imagens; i++){
+				using (SKBitmap bitmapEntrada = SKBitmap.Decode("C:\\Users\\mateus.freitas\\Downloads\\Atv3Cog\\Atividade 3\\Exercicio 1\\Exercicio1_"+i+".png"),
+				bitmapSaidaAritmetica = new SKBitmap(new SKImageInfo(bitmapEntrada.Width, bitmapEntrada.Height, SKColorType.Gray8))) {
+
+					unsafe {
+						byte* entrada = (byte*)bitmapEntrada.GetPixels();
+						byte* saida = (byte*)bitmapSaidaAritmetica.GetPixels();
+						int largura = bitmapEntrada.Width;
+						int altura = bitmapEntrada.Height;
+						bool considerar8vizinhos = true;
+						List<Forma> formas = new List<Forma>();
+
+						int pixelsTotais = bitmapEntrada.Width * bitmapEntrada.Height;
+
+						for (int e = 0, s = 0; s < pixelsTotais; e += 4, s++) {
+							if(entrada[e] == 255 && entrada[e+1] == 255 && entrada[e+2]== 255){
+								saida[s] = 255;
+							}else{
+								saida[s] = 0;
+							}
+						}
+
+						formas = Forma.DetectarFormas(saida, largura, altura, considerar8vizinhos);
+						objetos.Add("Exercicio1_"+i+".png", formas.Count);
+					}
+					using (FileStream stream = new FileStream("C:\\Users\\mateus.freitas\\Downloads\\Atv3Cog\\Atividade 3\\Exercicio 1\\Exercicio1_"+i+"_saida"+".png", FileMode.OpenOrCreate, FileAccess.Write)) {
+						bitmapSaidaAritmetica.Encode(stream, SKEncodedImageFormat.Png, 100);
+					}
+
+				}
+			}
+
+			var objetosordenados = objetos.OrderByDescending(kv => kv.Value).ToDictionary(kv => kv.Key, kv => kv.Value);
+			for (int i = 0; i < objetosordenados.Count; i++){
+    			Console.WriteLine(objetosordenados.ElementAt(i).Key + " - "+objetosordenados.ElementAt(i).Value);
+			}
+		}
+	}
+}
---
+++ Atividade 3/Exercicio 2/Program.cs
@@ -0,0 +1,81 @@
+﻿using SkiaSharp;
+
+namespace Projeto {
+    class Program {
+        static void Main(string[] args) {
+      
+            using (SKBitmap bitmapEntrada = SKBitmap.Decode("C:\\Users\\mateus.freitas\\Downloads\\Atv3Cog\\Atividade 3\\Exercicio 2\\Exercicio2.png"),
+                bitmapSaida = new SKBitmap(new SKImageInfo(bitmapEntrada.Width, bitmapEntrada.Height, SKColorType.Bgra8888))) {
+
+               
+                Console.WriteLine(bitmapEntrada.ColorType);
+                Console.WriteLine(bitmapSaida.ColorType);
+
+               
+                unsafe {
+                    byte* entrada = (byte*)bitmapEntrada.GetPixels();
+                    byte* saida = (byte*)bitmapSaida.GetPixels();
+                    int pixelsTotais = bitmapEntrada.Width * bitmapEntrada.Height;
+
+                    for (int e = 0, s = 0; s < pixelsTotais; e += 4, s++) {
+                      
+                        byte H, S, V;
+                        byte r = entrada[e + 2];
+                        byte g = entrada[e + 1];
+                        byte b = entrada[e];
+                        HSV(r, g, b, out H, out S, out V);
+
+                        // Verifica a luminosidade (V) e aplica uma máscara na imagem de saída
+                        if (V >= 85) {
+                            // Mantém a cor original se a luminosidade for suficientemente alta
+                            saida[e + 3] = entrada[e + 3];
+                            saida[e + 2] = entrada[e + 2];
+                            saida[e + 1] = entrada[e + 1];
+                            saida[e] = entrada[e];
+                        } else {
+                            // Torna o pixel preto se a luminosidade for baixa
+                            saida[e + 3] = 255;
+                            saida[e + 2] = 0;
+                            saida[e + 1] = 0;
+                            saida[e] = 0;
+                        }
+                    }
+                }
+
+          
+                using (FileStream stream = new FileStream("C:\\Users\\mateus.freitas\\Downloads\\Atv3Cog\\Atividade 3\\Exercicio 2\\Exercicio2_Saida.png", FileMode.OpenOrCreate, FileAccess.Write)) {
+                    bitmapSaida.Encode(stream, SKEncodedImageFormat.Png, 100);
+                }
+            }
+        }
+
+       
+        static void HSV(byte r, byte g, byte b, out byte h, out byte s, out byte v) {
+            double red = (double)r / 255.0;
+            double green = (double)g / 255.0;
+            double blue = (double)b / 255.0;
+            double maximo = Math.Max(Math.Max(red, green), blue);
+            double minimo = Math.Min(Math.Min(red, green), blue);
+            double H = 0;
+            double S = ((maximo == 0) ? 0 : ((maximo - minimo) / maximo));
+            double V = maximo;
+
+            if (maximo != minimo) {
+                if (maximo == red) {
+                    if (green >= blue)
+                        H = (60.0 * ((green - blue) / (maximo - minimo))) / 360.0;
+                    else
+                        H = ((60.0 * ((green - blue) / (maximo - minimo))) + 360.0) / 360.0;
+                } else if (maximo == green) {
+                    H = ((60.0 * ((blue - red) / (maximo - minimo))) + 120.0) / 360.0;
+                } else {
+                    H = ((60.0 * ((red - green) / (maximo - minimo))) + 240.0) / 360.0;
+                }
+            }
+
+            h = (byte)(255.0 * H);
+            s = (byte)(255.0 * S);
+            v = (byte)(255.0 * V);
+        }
+    }
+}
